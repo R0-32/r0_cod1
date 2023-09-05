4b79271e9c582c3107c0d9ab0c6135c945675596
R0-32 <wodrom@gmail.com>
2023-09-05 16:27:48 +0300

    auto-commit

---
+++ admin/DataBase/sql/Start-sqlLite.sql
@@ -0,0 +1,7 @@
+1.
+https://www.sqlite.org/download.html
+скачиваем длл Precompiled Binaries и тулзу
+распаковываем в одну директорию длл и sqlite3.exe
+
+2.
+
---
+++ pattern/r0_js/react-js/start-react.js
@@ -0,0 +1,16 @@
+0.
+node -v
+npm -v
+
+1.
+mkdir C:\Users\New\Downloads\topidato
+cd C:\Users\New\Downloads\topidato
+npx create-react-app my-react-project
+cd my-react-project
+dir
+tree
+npm start
+http://localhost:3000/
+
+Ctrl + C 
+npm run build
